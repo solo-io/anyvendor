8c4c2ce63b33ae34375709f1b74de457ae7f4c3d
EItanya <eitan.yarmush@solo.io>
2019-12-29 09:46:54 -0500

    added more comments about folder name

--- protodep/defaults.go
+++ protodep/defaults.go
@@ -7,5 +7,7 @@ const (
 	// default directory into which proto, and other files will be vendored.
 	// Originally this was meant to be the vendor directory, but clashes with the go vendor directory
 	// meant it would be easier for this to inhabit it's own folder
+	// See this section for more info: https://tip.golang.org/doc/go1.14#go-command
+	// This tool should not force to projects to build using vendor.
 	DefaultDepDir = ".proto_vendor"
 )
